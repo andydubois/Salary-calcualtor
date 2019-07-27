96e8da22dcff5bedac8e91f2308c3f5aa935ec32
Andy DuBois <andydubois89@gmail.com>
2019-07-27 12:17:40 -0500

    row deletion now updates total monthly cost properly

--- script.js
+++ script.js
@@ -45,24 +45,23 @@ function addEmployee() {
     // monthlySalary += (parseFloat(annualSalary/12));
     // console.log(monthlySalary);
 
-    
+
     // Clears all inputs on submission
     $('#firstName').val('');
     $('#lastName').val('');
     $('#emplId').val('');
     $('#title').val('');
-    $('#annSalary').val(''); 
-    //Calc monthly salary
+    $('#annSalary').val('');
     //cost per employee per month
     let monthlyCost = monthlyEmployeeCost(annualSalary);
     console.log('the monthly cost=' + monthlyCost);
     //total cost to the company for all employees
     let totalMonthlyCost = totalMonthly(htmlMonthlyCost, monthlyCost);
-     console.log('the totalMonthlyCost=' + totalMonthlyCost);
+    console.log('the totalMonthlyCost=' + totalMonthlyCost);
     //set global variable equal to monthly cost to company
     htmlMonthlyCost = totalMonthlyCost;
     console.log('htmlMonthlyCost=' + htmlMonthlyCost);
-    
+
     //Append value in DOM with total cost to company
     $('#totalMonthlySpan').text(htmlMonthlyCost.toFixed(2));
     //change background color if htmlMonthlyCost exceeds 20,000
@@ -74,7 +73,21 @@ function addEmployee() {
 function deleteRow() {
     let deleteValue = $('#deleteMe').text();
     console.log('deleteRow!');
+    //target row to delete
     $(this).parent().parent().remove();
     console.log("the value to delete is" + deleteValue);
-
+    //find value of number to subtract from total
+    let numberToSubtract = deleteValue / 12;
+    console.log('number to subtract =' + numberToSubtract);
+    //take value away from total
+    let finalValue = htmlMonthlyCost -= numberToSubtract;
+    console.log(finalValue);
+    //set global variable equal to new monthly cost value
+    htmlMonthlyCost = finalValue;
+    //append new value to DOM
+    $('#totalMonthlySpan').text(htmlMonthlyCost.toFixed(2));
+    //change background color from red back to nothing IF value drops below 20,000
+    if (htmlMonthlyCost < 20000) {
+        $('#totalMonthlySpan').css('background-color', '');
+    };
 }
